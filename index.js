2164e9592e829cbed967d951fad0b77361ee6dbe
JoseCorreaMorales <jose.correax@gmail.com>
2023-08-31 10:38:53 -0600

    services GET

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 
+const Service = require('./src/service');
+
 const app = express();
 const PORT = 3000
 
 /* 1 - route, 2- controller */
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello 🌍!'
+        message: 'Welcome to the web server!'
     })
 })
 
-app.listen(PORT, () =>{ 
+app.get('/users', (req, res) => {
+    res.json({
+        message: 'List of users',
+        body: Service.getUsers(),
+    })
+})
+
+app.listen(PORT, () => { 
     console.log(`Server listening on http://localhost:${PORT}`)
 })
